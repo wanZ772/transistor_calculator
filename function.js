a29edd411dc627e64d5684b88993f4af8665f5a7
Muhammad Najwan <najwanhassan03@gmail.com>
2024-05-31 17:26:27 +0800

    DC Analysis

---
+++ function.js
@@ -0,0 +1,43 @@
+function thevenin_voltage(R1, R2, Vcc) {
+    return Vcc*Rb2/(Rb2+Rb1);
+}
+function thevenin_resistance(R1, R2)    {
+    return 1/(1/R1 + 1/R2);
+}
+function calc() {
+    beta = parseFloat(document.getElementById("beta").value);
+    Vbe = parseFloat(document.getElementById("Vbe").value);
+    Rb1 = parseFloat(document.getElementById("Rb1").value);
+    Rb2 = parseFloat(document.getElementById("Rb2").value);
+    Vcc = parseFloat(document.getElementById("vcc").value);
+    Rc = parseFloat(document.getElementById("Rc").value);
+    Re = parseFloat(document.getElementById("Re").value);
+    if (Rb2 != 0)   {
+        Vth = thevenin_voltage(Rb1, Rb2, Vcc);
+        
+        Rb = thevenin_resistance(parseFloat(Rb1), parseFloat(Rb2));
+        Ib = (Vth - Vbe) / (Rb + beta * Re);
+    }   else    {
+        Rb = parseFloat(Rb1);
+        Ib = (Vcc - Vbe) / (Rb + beta * Re);
+    }
+    
+    
+    
+    
+    Ic = Ie = beta * Ib;
+    Vc = Vcc - Ic*Rc;
+    Ve = Ie*Re;
+    Vb = Vbe + Ie*Re;
+    Vce = Vcc - Ic*Rc - Ie*Re;
+    
+    document.getElementById("Ib_value").innerHTML = Ib + "A";
+    document.getElementById("Ic_value").innerHTML = Ic + "A";
+    document.getElementById("Ie_value").innerHTML = Ie + "A";
+    
+    document.getElementById("Vb_value").innerHTML = Vb + "V";
+    document.getElementById("Vc_value").innerHTML = Vc + "V";
+    document.getElementById("Ve_value").innerHTML = Ve + "V";
+    document.getElementById("Vce_value").innerHTML = Vce + "V";
+
+}
